1e0558ac6009e2a2f914141276b67f0f689d184f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-07-19 11:11:56 +0200

    star fin

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -3,35 +3,37 @@
 
 	var app = angular.module('ors-star', []);
 
-	app.directive('orsStar', function() {
+	app.directive('orsStar', ['$compile', function($compile) {
 		return {
 			restrict: 'E',
 			scope: {
 				n: '=note'
 			},
 			link: function(scope, element) {
+				scope.update = function(note) {
+					scope.n = note;
+				};
 			
 				scope.$watch('n', function() {
-					console.log('orsStar', arguments);
 					var note = Number(scope.n);
 					note = isNaN(note) ? 4 : note;
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
-					console.log('note', note);
 					var html = '';
 					for (var i = 0; i < note; i++) {
-						html += '<img src="ors-star/img/yellow_star.png" />';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="ors-star/img/yellow_star.png" />';
 					}
 					for (var i = note; i < 5; i++) {
-						html += '<img src="ors-star/img/white_star.png" />';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="ors-star/img/white_star.png" />';
 					}
 					element.html(html);
+					$compile(element.contents())(scope);
 				
 				});
 			
 				
 			}
 		};
-	});
+	}]);
 	
 })();
