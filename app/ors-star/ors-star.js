b793769335866ba81e090f99cc08c3e2f65927a6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-07-19 10:17:46 +0200

    star visible

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,8 +6,17 @@
 	app.directive('orsStar', function() {
 		return {
 			restrict: 'E',
-			link: function() {
+			link: function(scope, element, attrs) {
 				console.log('orsStar', arguments);
+				var note = 4;
+				var html = '';
+				for (var i = 0; i < note; i++) {
+					html += '<img src="ors-star/img/yellow_star.png" />';
+				}
+				for (var i = note; i < 5; i++) {
+					html += '<img src="ors-star/img/white_star.png" />';
+				}
+				element.html(html);
 			}
 		};
 	});
